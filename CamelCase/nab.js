364348a09d0018ea3e06dec00d65505bcccdd571
Nabeel <38478892+nabeelmumtaz2@users.noreply.github.com>
2018-10-08 13:09:45 -0400

    Create CamelCase

---
+++ CamelCase/nab.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/voca/1.4.0/voca.js"></script>
+<script>
+function camelCase(str) {
+
+   return v.camelCase(str);
+
+}
+
+console.log(camelCase("An empty string was provided but not returned"));
+console.log(camelCase("the_stealth_warrior"));
+console.log(camelCase("The-Stealth-Warrior"));
+console.log(camelCase("A-B-C"));
+
+</script>
+</head>
+<body>
+</body>
+</html>
+
